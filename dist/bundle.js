b92c28fbb82242a38be5505df5b0d5524755983e
damianszocik <damian.szocik@gmail.com>
2018-05-15 19:02:37 +0200

    added speech recognition api exeption/support handling

--- dist/bundle.js
+++ dist/bundle.js
@@ -775,36 +775,36 @@ exports.init = init;
 __webpack_require__(/*! ./script.js */ "./src/script.js");
 
 function init() {
-    try {
-        var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-        var recognition = new SpeechRecognition();
-    } catch (error) {
-        console.error(error);
-    }
-
-    recognition.onstart = function () {
-        animations.listenVoiceSearchButton("start");
-    };
-
-    recognition.onerror = function (event) {
-        animations.listenVoiceSearchButton("stop");
-        if (event.error == "no-speech") {
-            alert("No speech was detected. Try again.");
-        };
-    };
-
-    recognition.onresult = function (event) {
-        animations.listenVoiceSearchButton("stop");
-        elements.searchInput.focus();
-        setTimeout(function () {
-            console.log(event.results[0][0].transcript);
-            elements.searchInput.value = event.results[0][0].transcript;
-            dataProcessing.fetchData(elements.searchInput.value);
-        }, 400);
-    };
-
     elements.voiceSearchButton.addEventListener("click", function () {
-        recognition.start();
+        try {
+            var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+            var recognition = new SpeechRecognition();
+
+            recognition.onstart = function () {
+                animations.listenVoiceSearchButton("start");
+            };
+
+            recognition.onerror = function (event) {
+                animations.listenVoiceSearchButton("stop");
+                if (event.error == "no-speech") {
+                    alert("No speech was detected. Try again.");
+                };
+            };
+
+            recognition.onresult = function (event) {
+                animations.listenVoiceSearchButton("stop");
+                elements.searchInput.focus();
+                setTimeout(function () {
+                    console.log(event.results[0][0].transcript);
+                    elements.searchInput.value = event.results[0][0].transcript;
+                    dataProcessing.fetchData(elements.searchInput.value);
+                }, 400);
+            };
+            recognition.start();
+        } catch (error) {
+            alert("Can't access speech recognition API. Your browser may not support that functionality.");
+            console.log(error);
+        }
     });
 }
 
